a3ab28923f0068992316bea1e42d537c4584f6a1
esteban.arroyo <esteban.arroyo@epita.fr>
2024-10-24 14:42:42 +0200

    [Dev] fix CI/CD

--- vite.config.js
+++ vite.config.js
@@ -23,23 +23,9 @@ export default defineConfig({
     plugins: [
         viteStaticCopy({
             targets: [
-                {src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/'},
-                {src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/'},
                 {
-                    src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.js',
-                    dest: 'jsm/libs/draco/gltf'
-                },
-                {
-                    src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.wasm',
-                    dest: 'jsm/libs/draco/gltf/'
-                },
-                {
-                    src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_encoder.js',
-                    dest: 'jsm/libs/draco/gltf/'
-                },
-                {
-                    src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js',
-                    dest: 'jsm/libs/draco/gltf/'
+                    src: 'node_modules/*',
+                    dest: 'jsm/libs/',
                 }
             ]
         }),
