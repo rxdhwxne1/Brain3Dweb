97b146753046cb199eee119c5910f32ac38fd088
esteban.arroyo <esteban.arroyo@epita.fr>
2024-10-24 21:50:01 +0200

    [Dev] fix type to image

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
     base: './',
-    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.ico', '**/*.json'],
     clearScreen: false,
     build: {
         sourcemap: true
