a5c3d68a92885ff76fb854df4755590bcb5e6fa1
esteban.arroyo <esteban.arroyo@epita.fr>
2024-10-28 19:16:31 +0100

    [fix] modify the class color to add tolerance to get the color where we click

--- src/utils/color.js
+++ src/utils/color.js
@@ -7,16 +7,20 @@ export class color {
         this.b = b;
     }
 
+    isInRange(value, target, tolerance = 10) {
+        return Math.abs(value - target) <= tolerance;
+    }
+
     get_color() {
-        if (this.r === 52 && this.g === 182 && this.b === 56) {
+        if (this.isInRange(this.r, 52) && this.isInRange(this.g, 182) && this.isInRange(this.b, 56)) {
             return "green";
-        } else if (this.r === 52 && this.g === 166 && this.b === 158) {
+        } else if (this.isInRange(this.r, 52) && this.isInRange(this.g, 166) && this.isInRange(this.b, 158)) {
             return "blue";
-        } else if (this.r === 117 && this.g === 48 && this.b === 19 || this.r === 117 && this.g === 47 && this.b === 21) {
+        } else if (this.isInRange(this.r, 117) && this.isInRange(this.g, 47) && this.isInRange(this.b, 19)) {
             return "brown";
-        } else if (this.r === 224 && this.g === 221 && this.b === 64) {
+        } else if (this.isInRange(this.r, 224) && this.isInRange(this.g, 221) && this.isInRange(this.b, 64)) {
             return "yellow";
-        } else if (this.r === 195 && this.g === 43 && this.b === 42) {
+        } else if (this.isInRange(this.r, 195) && this.isInRange(this.g, 43) && this.isInRange(this.b, 42)) {
             return "red";
         } else {
             return "color not found";
